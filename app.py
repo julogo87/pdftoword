6acb308dabb74c26b8cb818c314b2f2998dae0b8
julogo87 <julogo87@hotmail.com>
2024-08-26 14:43:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, send_file, redirect, url_for
 import os
 from werkzeug.utils import secure_filename
-from PyPDF2 import PdfReader, PdfWriter
 from io import BytesIO
+import pdfplumber
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -11,18 +13,27 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 def replace_text_in_pdf(file_path, old_text, new_text):
-    reader = PdfReader(file_path)
-    writer = PdfWriter()
-    for page in reader.pages:
-        text = page.extract_text()
-        if old_text in text:
-            text = text.replace(old_text, new_text)
-            page_text = page.extract_text()
-            page_text = page_text.replace(old_text, new_text)
-            writer.add_page(page)
-    output = BytesIO()
-    writer.write(output)
-    return output
+    with pdfplumber.open(file_path) as pdf:
+        output = BytesIO()
+        c = canvas.Canvas(output, pagesize=letter)
+
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text and old_text in text:
+                text = text.replace(old_text, new_text)
+            
+            # Escribir el texto modificado en un nuevo PDF
+            text_lines = text.split("\n")
+            y = 750
+            for line in text_lines:
+                c.drawString(10, y, line)
+                y -= 15
+
+            c.showPage()  # Crear una nueva página en el PDF
+
+        c.save()
+        output.seek(0)
+        return output
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -38,7 +49,6 @@ def index():
 
             if old_text and new_text:
                 output = replace_text_in_pdf(file_path, old_text, new_text)
-                output.seek(0)
                 return send_file(output, as_attachment=True, download_name=filename, mimetype='application/pdf')
 
             return send_file(file_path, as_attachment=True)
@@ -48,4 +58,5 @@ def index():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
 
