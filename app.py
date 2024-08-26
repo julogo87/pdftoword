69328d1bddb46f0c503125536dda85879b9290b3
julogo87 <julogo87@hotmail.com>
2024-08-26 15:32:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from PyPDF2 import PdfReader, PdfWriter
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
+import os
 
 app = Flask(__name__)
 
@@ -44,6 +45,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
